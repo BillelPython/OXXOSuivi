7381f39dd5d0d4167eff18da4f885560f6595d4d
billel.benmakhlouf <billel.benmakhlouf@gmail.com>
2025-10-28 14:16:44 +0100

    Fix ALLOWED_HOSTS for Render

--- OXXOSuivi/settings.py
+++ OXXOSuivi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=wo7lhrz__y)n*(!_e(t#jngv$7zuaow^cbk@4*qg(&m$*q@#_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
